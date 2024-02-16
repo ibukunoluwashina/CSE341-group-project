99d88e075221fd7fedd1d9d61a6fc7faae6ed64c
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2024-02-16 01:00:05 +0100

    genre id validation fixed

--- middleware/validate.js
+++ middleware/validate.js
@@ -61,7 +61,7 @@ const storeBook = (req, res, next) => {
   const validationRule = {
     title: "required|string",
     authorId: "required|string",
-    genreId: "required|date",
+    genreId: "required|integer",
     publicationYear: "required|integer",
     isbn: "required|string",
     isAvailable: "required|boolean",
